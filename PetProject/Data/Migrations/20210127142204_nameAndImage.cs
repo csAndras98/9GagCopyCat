6929e5b43e1771f1a72dab1e9f85e26639ac4911
tompuljalmeg <tompuljalmeg@gmail.com>
2021-01-27 15:28:55 +0100

    new migration

---
+++ PetProject/Data/Migrations/20210127142204_nameAndImage.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PetProject.Data.Migrations
+{
+    public partial class nameAndImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Names",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CharName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Names", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Portraits",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Image = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Portraits", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Names");
+
+            migrationBuilder.DropTable(
+                name: "Portraits");
+        }
+    }
+}
