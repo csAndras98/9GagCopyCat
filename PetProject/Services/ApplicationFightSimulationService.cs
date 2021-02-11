27dfda0862f209fe40b6c759420f68ad09e751c0
tompuljalmeg <tompuljalmeg@gmail.com>
2021-02-11 18:16:01 +0100

    FightSimulationService

---
+++ PetProject/Services/ApplicationFightSimulationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetProject.Services
+{
+    public class ApplicationFightSimulationService
+    {
+        private readonly ApplicationDbService _dbService;
+        public ApplicationFightSimulationService(ApplicationDbService dbService)
+        {
+            _dbService = dbService;
+        }
+    }
+}
